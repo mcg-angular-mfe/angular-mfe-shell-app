82a50dd1bf949579f3e252a5ea2df114ee6a142c
migodau <migodau@gmail.com>
2022-01-09 14:06:51 -0300

    shell: initial structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: 'mfe1',
+    loadChildren: () =>
+      import('mfe1/Module').then((m) => m.Mfe1Module),
+  }
+  // {
+  //   path: 'mfe1',
+  //   loadChildren: () => loadRemoteModule({
+  //       type: 'module',
+  //       remoteEntry: 'http://localhost:3001/remoteEntry.js',
+  //       exposedModule: './Module'
+  //     })
+  //     .then(m => m.AppModule) 
+  // },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
