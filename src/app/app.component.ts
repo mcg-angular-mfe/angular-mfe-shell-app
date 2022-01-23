913d5261c51e296df5dc33f7ee9fd65adeb53755
migodau <migodau@gmail.com>
2022-01-23 15:24:42 -0300

    routing loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,9 +9,39 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'shell';
+  loading = false;
 
-  constructor(private translateService: TranslateService) {
+  constructor(
+    private translateService: TranslateService,
+    private router: Router,
+  ) {
+    // i18n init
     this.translateService.setDefaultLang('pt');
     this.translateService.use('pt');
+
+    this.loadingHandler();
+  }
+
+  loadingHandler() {
+    this.router.events
+    .subscribe((event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          // setTimeout(() => this.loading = false, 1000); // just a test
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
   }
 }
